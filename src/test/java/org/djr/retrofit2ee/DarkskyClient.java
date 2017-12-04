f03ab0425f3ab5477b7d6c37520ec1830955d883
djr4488 <djr4488@gmail.com>
2017-12-04 09:10:42 -0600

    just some refactoring bits here and there

--- src/test/java/org/djr/retrofit2ee/DarkskyClient.java
+++ src/test/java/org/djr/retrofit2ee/DarkskyClient.java
@@ -6,8 +6,6 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface DarkskyClient {
-	@GET("forecast/{key}/{latitude},{longitude}")
-	Call<DarkskyResponse> getForecast(@Path("key") String key, @Path("latitude") String latitude,
-									   @Path("longitude") String longitude, @Query("lang") String language,
-									   @Query("units") String units, @Query("timezone") String timezone);
+	@GET("country/get/iso2code/{alpha2code}")
+	Call<DarkskyResponse> getForecast(@Path("alpha2code") String alpha2code);
 }
