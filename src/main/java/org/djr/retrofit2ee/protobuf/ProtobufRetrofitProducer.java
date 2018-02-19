71309c7d4b867dbb6fd712b5963a4e3327f6c4e6
djr4488 <djr4488@gmail.com>
2018-02-18 22:49:47 -0600

    add licenses remove some unneeded empty lines in methods

---
+++ src/main/java/org/djr/retrofit2ee/protobuf/ProtobufRetrofitProducer.java
@@ -0,0 +1,55 @@
+package org.djr.retrofit2ee.protobuf;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import org.djr.retrofit2ee.RetrofitProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import retrofit2.Retrofit;
+import retrofit2.converter.protobuf.ProtoConverterFactory;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+import java.util.Properties;
+
+public class ProtobufRetrofitProducer {
+    private static Logger log = LoggerFactory.getLogger(ProtobufRetrofitProducer.class);
+    @Inject
+    @RetrofitProperties
+    private Properties properties;
+
+    @Produces
+    @RetrofitProtobuf
+    public Retrofit getClient(InjectionPoint injectionPoint)
+            throws NoSuchFieldException, InstantiationException, IllegalAccessException {
+        RetrofitProtobuf protobufClientConfig = injectionPoint.getAnnotated().getAnnotation(RetrofitProtobuf.class);
+        log.debug("getClient() injecting retrofit protobuf client with annotation:{}", protobufClientConfig);
+        String baseUrlPropertyName = protobufClientConfig.baseUrlPropertyName();
+        String captureTrafficLogsPropertyName = protobufClientConfig.captureTrafficLogsPropertyName();
+        String baseUrl = properties.getProperty(baseUrlPropertyName);
+        Boolean enableTrafficLogging =
+                Boolean.parseBoolean(properties.getProperty(captureTrafficLogsPropertyName, "FALSE"));
+        return getTransport(baseUrl, enableTrafficLogging);
+    }
+
+    private Retrofit getTransport(String baseUrl, boolean enableTrafficLogging) {
+        log.debug("getTransport() baseUrl:{}, enableTrafficLogging:{}", baseUrl, enableTrafficLogging);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        setLoggingInterceptor(enableTrafficLogging, httpClient);
+        Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
+                .addConverterFactory(ProtoConverterFactory.create())
+                .baseUrl(baseUrl)
+                .client(httpClient.build());
+        retrofitBuilder.client(httpClient.build());
+        return retrofitBuilder.build();
+    }
+
+    private void setLoggingInterceptor(boolean enableTrafficLogging, OkHttpClient.Builder builder) {
+        if (enableTrafficLogging) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(loggingInterceptor);
+        }
+    }
+}
