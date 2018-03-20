2f31012f3a239d150295cbb48a0024a8958990c8
djr4488 <djr4488@gmail.com>
2018-03-20 07:49:09 -0500

    added Guava Optional support and test cases fixed some logging

--- src/main/java/org/djr/retrofit2ee/guava/GuavaRetrofitProducer.java
+++ src/main/java/org/djr/retrofit2ee/guava/GuavaRetrofitProducer.java
@@ -1,31 +1,31 @@
-package org.djr.retrofit2ee.scalar;
+package org.djr.retrofit2ee.guava;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import org.djr.retrofit2ee.RetrofitProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import retrofit.converter.guava.GuavaOptionalConverterFactory;
 import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Inject;
 import java.util.Properties;
 
-public class ScalarsRetrofitProducer {
-    private static Logger log = LoggerFactory.getLogger(ScalarsRetrofitProducer.class);
+public class GuavaRetrofitProducer {
+    private static Logger log = LoggerFactory.getLogger(GuavaRetrofitProducer.class);
     @Inject
     @RetrofitProperties
     private Properties properties;
 
     @Produces
-    @RetrofitScalar
+    @RetrofitGuava
     public Retrofit getClient(InjectionPoint injectionPoint) {
-        RetrofitScalar scalarClientConfig = injectionPoint.getAnnotated().getAnnotation(RetrofitScalar.class);
-        log.debug("getClient() injecting retrofit scalar client with annotation:{}", scalarClientConfig);
-        String baseUrlPropertyName = scalarClientConfig.baseUrlPropertyName();
-        String captureTrafficLogsPropertyName = scalarClientConfig.captureTrafficLogsPropertyName();
+        RetrofitGuava guavaClientConfig = injectionPoint.getAnnotated().getAnnotation(RetrofitGuava.class);
+        log.debug("getClient() injecting retrofit guava client with annotation:{}", guavaClientConfig);
+        String baseUrlPropertyName = guavaClientConfig.baseUrlPropertyName();
+        String captureTrafficLogsPropertyName = guavaClientConfig.captureTrafficLogsPropertyName();
         String baseUrl = properties.getProperty(baseUrlPropertyName);
         Boolean enableTrafficLogging =
                 Boolean.parseBoolean(properties.getProperty(captureTrafficLogsPropertyName, "FALSE"));
@@ -37,7 +37,7 @@ public class ScalarsRetrofitProducer {
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
         setLoggingInterceptor(enableTrafficLogging, httpClient);
         Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
-                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GuavaOptionalConverterFactory.create())
                 .baseUrl(baseUrl)
                 .client(httpClient.build());
         retrofitBuilder.client(httpClient.build());
---
+++ src/main/java/org/djr/retrofit2ee/guava/RetrofitGuava.java
@@ -0,0 +1,20 @@
+package org.djr.retrofit2ee.guava;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Qualifier
+@Retention(RUNTIME)
+@Target({METHOD, FIELD, PARAMETER, TYPE})
+public @interface RetrofitGuava {
+    @Nonbinding String captureTrafficLogsPropertyName() default "";
+    @Nonbinding String baseUrlPropertyName() default "";
+}
